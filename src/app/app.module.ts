1870d7b4e735bcbd2787edc72fdd2e26cf0bc863
Thomas Perles <thomas.perles@gmail.com>
2023-03-21 23:39:59 +0100

    Ajout de composants PrimeNG Un password pour la difficulté du mot de passe et que l'on peut rendre visible dans le composant d'authentification Un rating de 5 étoiles dans le formulaire LivreAvis Un input group pour la recherche dans le composant header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './component/accueil/accueil.component';
-import { HeaderComponent } from './component/header/header.component';
-import { LivresComponent } from './component/livres/livres.component';
-import { LivreItemComponent } from './component/livre-item/livre-item.component';
-import { LivreFormComponent } from './component/livre-form/livre-form.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LivreDetailComponent } from './component/livre-detail/livre-detail.component';
 import { AuthentificationComponent } from './component/authentification/authentification.component';
-import { LivreEmpruntFormComponent } from './component/livre-emprunt-form/livre-emprunt-form.component';
+import { HeaderComponent } from './component/header/header.component';
 import { LivreAvisFormComponent } from './component/livre-avis-form/livre-avis-form.component';
-import { CalendarModule } from 'primeng/calendar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LivreDetailComponent } from './component/livre-detail/livre-detail.component';
+import { LivreEmpruntFormComponent } from './component/livre-emprunt-form/livre-emprunt-form.component';
+import { LivreFormComponent } from './component/livre-form/livre-form.component';
+import { LivreItemComponent } from './component/livre-item/livre-item.component';
+import { LivresComponent } from './component/livres/livres.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +41,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     HttpClientModule,
     CalendarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PasswordModule,
+    RatingModule,
+    InputTextModule,
+    RippleModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
